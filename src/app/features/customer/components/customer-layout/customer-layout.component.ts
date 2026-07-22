67cc1d8001ac59c93f0b806eced058c7d051c9fc
TELEPATY_ORG\yzoabi <yzoabi@telepaty.it>
2026-07-22 10:04:13 +0300

    translation

--- src/app/features/customer/components/customer-layout/customer-layout.component.ts
+++ src/app/features/customer/components/customer-layout/customer-layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { CustomerNavbarComponent } from '../customer-navbar/customer-navbar.component';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-customer-layout',
   standalone: true,
-  imports: [RouterLink, RouterOutlet, CustomerNavbarComponent],
+  imports: [RouterLink, RouterOutlet, CustomerNavbarComponent,TranslatePipe],
   templateUrl: './customer-layout.component.html',
   styleUrl: './customer-layout.component.scss',
 })
